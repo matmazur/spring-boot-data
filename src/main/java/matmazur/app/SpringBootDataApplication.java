6a0086ea527886d90abc0c8bf5385be18801e889
Bingo Bobo <matmazur90@gmail.com>
2018-12-11 21:15:06 +0100

    configured application.properties, moved Person and Person Repository from previous project without boot

--- src/main/java/matmazur/app/SpringBootDataApplication.java
+++ src/main/java/matmazur/app/SpringBootDataApplication.java
@@ -1,12 +1,17 @@
-package com.matmazur.springbootdata;
+package matmazur.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootDataApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootDataApplication.class, args);
+
+        ConfigurableApplicationContext context =  SpringApplication.run(SpringBootDataApplication.class, args);
+
+
+
     }
 }
---
+++ src/main/java/matmazur/model/Person.java
@@ -0,0 +1,72 @@
+package matmazur.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_person")
+    private Long id;
+    private String name;
+    private String surname;
+    private Integer age;
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public Person(String name, String surname, Integer age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "com.matmazur.model.Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public static class Builder {
+
+        private String name;
+        private String surname;
+        private Integer age;
+
+        public Builder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder withSurname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        public Builder withAge(Integer age) {
+            this.age = age;
+            return this;
+        }
+
+        public Person build(){
+            return new Person(name,surname,age);
+        }
+    }
+}
