f0c61c277da07d1c93ad222766468d350186bf1c
Bingo Bobo <matmazur90@gmail.com>
2018-12-13 12:59:50 +0100

    changed extended interface for repository, added paging mechanism in main class, showing first 2 records ascending going by name

--- src/main/java/matmazur/SpringBootDataApplication.java
+++ src/main/java/matmazur/SpringBootDataApplication.java
@@ -5,6 +5,9 @@ import matmazur.repository.PersonRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,12 +22,22 @@ public class SpringBootDataApplication {
         Person p1 = Person.builder().withName("Jack").withSurname("Strong").withAge(36).build();
         Person p2 = Person.builder().withName("John").withSurname("Doe").withAge(44).build();
         Person p3 = Person.builder().withName("Amy").withSurname("Ashley").withAge(22).build();
-        List<Person> list = Arrays.asList(p1, p2, p3);
+        Person p4 = Person.builder().withName("Jack").withSurname("Strong").withAge(36).build();
+        Person p5 = Person.builder().withName("John").withSurname("Doe").withAge(44).build();
+        Person p6 = Person.builder().withName("Amy").withSurname("Ashley").withAge(22).build();
+        List<Person> list = Arrays.asList(p1, p2, p3, p4, p5, p6);
         PersonRepository repo = context.getBean(PersonRepository.class);
         repo.saveAll(list);
 
 
         repo.findAll().forEach(System.out::println);
+        System.out.println(repo.count());
+        System.out.println(repo.existsById(4L));
+
+
+        Page<Person> people = repo.findAll(PageRequest.of(0, 2, Sort.Direction.ASC, "name"));
+        people.getContent().forEach(System.out::println);
+
 
     }
 }
--- src/main/java/matmazur/repository/PersonRepository.java
+++ src/main/java/matmazur/repository/PersonRepository.java
@@ -1,9 +1,9 @@
 package matmazur.repository;
 
 import matmazur.model.Person;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonRepository extends CrudRepository<Person, Long> {
+public interface PersonRepository extends  PagingAndSortingRepository<Person,Long> {
 }
